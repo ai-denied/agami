f279c6ead84039a54050c7d764431cf6369cbb92
이어진 <amvnfr@gmail.com>
2026-05-15 11:35:08 +0900

    260515 v1
    크기 반응형 디자인
    jwt 토큰
    배포

--- Develop/Frontend/src/pages/Price.jsx
+++ Develop/Frontend/src/pages/Price.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
-import "./price.css";
+import "./Price.css";
 import BubbleBtn from "../components/BubbleBtn";
 import WaveBg from "../components/WaveBg";
 import LiquidGlass from "../components/LiquidGlass";
