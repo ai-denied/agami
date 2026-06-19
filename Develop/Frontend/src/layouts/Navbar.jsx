9bf7f4ca3c60bf01272b4f9bde85f205f0d765b2
ImBazzi <amvnfr@gmail.com>
2026-06-19 09:54:00 +0900

    모바일 반응형 추가

--- Develop/Frontend/src/layouts/Navbar.jsx
+++ Develop/Frontend/src/layouts/Navbar.jsx
@@ -38,9 +38,9 @@ const Navbar = () => {
     } catch (e) { 
       console.error(e); 
     }
-    setUser(null); // 전역 Auth 상태 초기화
-    localStorage.removeItem("accessToken"); // clear() 대신 인증 토큰만 삭제하여 테마(theme) 설정은 유지시킴
-    navigate("/"); // 부드러운 화면 전환 (SPA 방식)
+    
+    localStorage.removeItem("accessToken");
+    window.location.href = "/"; 
   };
 
   if (isFirstVisit === null) return null;
